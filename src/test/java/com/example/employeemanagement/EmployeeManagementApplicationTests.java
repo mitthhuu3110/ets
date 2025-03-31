9b9d6332e876b9a4d481c15d16801e8b7687c556
mitthhuu3110 <3110mithilesh@gmial.com>
2025-03-31 11:50:43 +0530

    initial commit with all the code changes

---
+++ src/test/java/com/example/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EmployeeManagementApplication.class)
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
